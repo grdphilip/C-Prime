4a5aaf79114dea6ac7b5ca14c7f4a1ca2915b5d5
grdphilip <philiprettig11@gmail.com>
2022-01-27 22:36:53 +0100

    Great success

---
+++ Chapter2/chapterTwoOne.cpp
@@ -0,0 +1,13 @@
+/* 
+In C++, as in most programming languages, the type of an object determines what operations can be performed on it. 
+Some languages, such as Smalltalk and Python, check types at run time. In contrast, C++ is a statically typed language; 
+type checking is done at compile time. As a consequence, the compiler must know the type of every name used in the program.
+
+Perhaps the most important feature in C++ is the class, which lets programmers define their own types. 
+In C++ such types are some- times called “class types” to distinguish them from the types that are built into the language. 
+
+Types are fundamental to any program: They tell us what our data mean and what operations we can perform on those data.
+Different types are specified at 2.1.1
+*/
+
+
