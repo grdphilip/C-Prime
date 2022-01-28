5c8fbd3f6567c5ef21a21eacc03e1f743423ba71
grdphilip <philiprettig11@gmail.com>
2022-01-28 08:33:04 +0100

    ch2

--- Chapter2/chapterTwoOne.cpp
+++ Chapter2/chapterTwoOne.cpp
@@ -10,4 +10,10 @@ Types are fundamental to any program: They tell us what our data mean and what o
 Different types are specified at 2.1.1
 */
 
+bool b = 42; // b is true
+int i = b; // i has value 1
+i = 3.14; // i has value 3
+double pi = i; // pi has value 3.0
+unsigned char c = .1; // assuming 8-bit chars, c has value 255
+signed char c2 = 256; // assuming 8-bit chars, the value of c2 is undefined
 
